13c2e5baed3c345984650139483f4d019b5a9b71
Laura <lauraelena0804@gmail.com>
2019-04-04 22:03:20 +0200

    adds count words that starts with vowels

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -4,6 +4,7 @@ public class WordCounter {
 
     private String text;
     private ArrayList<String> differentWords;
+    private Integer countVocals;
 
     public WordCounter(String text){
         this.text = text;
@@ -25,6 +26,23 @@ public class WordCounter {
     private String[] prepareInputForCounting(String text){
         String temporalText = this.cleanInput(text);
         return temporalText.split(" ");
+    }
+
+    public Integer CountWordsBeginingWithVocals(){
+        String[] words = this.prepareInputForCounting(this.text);
+
+        for (String word: words) {
+            String lowerCaseWord= word.toLowerCase();
+            if(isVocal(lowerCaseWord.get(0))){
+                this.countVocals++;
+            }
+        }
+        return this.countVocals;
+    }
+
+    private String isVocal(char c){
+        String regexVocals = "/[aeiou]/";
+        return c.equals(regexVocals);
     }
 
     private String cleanInput(String input){
