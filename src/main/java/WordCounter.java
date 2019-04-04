55b76d2f81c515a40bffc7b958f64a11a5171742
Alejandro Enríquez <alexalejandroem@gmail.com>
2019-04-04 19:20:22 +0200

    Changed text

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -34,7 +34,7 @@ public class WordCounter {
     }
 
     public static void main(String[] args){
-        String text = "Laura guapa laura guapa";
+        String text = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500";
         WordCounter wordCounter = new WordCounter(text);
         System.out.println("The number of words is: "+ wordCounter.countDifferentWords());
     }
